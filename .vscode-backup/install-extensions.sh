1e3dc3350217c9b7b1b0a6a1c7af702a498ecbe1
Jeremy Muñoz Torres <JMunoz@contractor.nexstardigital.com>
2021-12-11 16:26:02 -0400

    🔌 Updated VSCode plugins

--- .vscode-backup/install-extensions.sh
+++ .vscode-backup/install-extensions.sh
@@ -6,6 +6,7 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-rename-tag
 code --install-extension GitHub.copilot
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
@@ -13,6 +14,7 @@ code --install-extension ms-vscode.vscode-typescript-next
 code --install-extension patbenatar.advanced-new-file
 code --install-extension persoderlind.vscode-phpcbf
 code --install-extension PKief.material-icon-theme
+code --install-extension rangav.vscode-thunder-client
 code --install-extension shevaua.phpcs
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension streetsidesoftware.code-spell-checker-spanish
